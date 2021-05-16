39552a24c04670c6cfd0b1bd227275710ba48f5c
Andrew Smith <smith.andrew.l@gmail.com>
2021-05-16 16:11:06 -0500

    Made the last method tail recursive.

--- src/main/scala/DSLinkedList.scala
+++ src/main/scala/DSLinkedList.scala
@@ -1,24 +1,25 @@
+import scala.annotation.tailrec
+
 class DSLinkedList[T]:
   class Node[T](var data: T, var next: Node[T])
 
   var head: Node[T] = null
 
-
   private def last(): Node[T] =
-    var current: Node[T] = head
+    @tailrec
+    def inner(current: Node[T]): Node[T] =
+      if current.next == null then
+        current
+      else
+        inner(current.next)
 
-    while current.next != null do
-      current = current.next
-
-    current
+    inner(head)
 
   def insert(data: T) =
     if head == null then
       head = Node(data, null)
     else
-      var last: Node[T] = this.last()
-
-      last.next = Node(data, null)
+      this.last().next = Node(data, null)
 
   override def toString: String =
     var out: Seq[String] = Seq()
